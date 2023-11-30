3fa7630506b2d293ffcf7f96180b63942d95b045
Ojas Pratap Singh <128834924+Ojas-Pratap-Singh@users.noreply.github.com>
2023-11-30 10:47:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 let browserName = navigator.appName;
 let version = navigator.appVersion;
-let divContainer = document.getElementById("browser_info");
+let divContainer = document.getElementById("browser-info");
 divContainer.innerHTML = "You are using " + browserName + " version " + version;
